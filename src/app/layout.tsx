d0bcfa9a8542b66be7f8196724705e99620e5644
Sheraz Hussain <sherazhussainofficial2@gmail.com>
2025-10-24 01:14:46 +0000

    lets do the same for this as well (_for element span_)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,8 +18,8 @@ function Header() {
     <header className="bg-card/80 backdrop-blur-sm sticky top-0 z-40 border-b">
         <div className="max-w-7xl mx-auto flex items-center justify-between p-4">
             <Link href="/" className="flex items-center gap-1">
-                <span className="font-headline font-bold text-2xl tracking-tighter">
-                  blogify<span className="text-primary">.blog</span>
+                <span className="font-headline text-2xl tracking-tighter">
+                  <span className="font-bold">blogify</span><span className="text-primary">.blog</span>
                 </span>
             </Link>
             <nav className="flex items-center gap-2 sm:gap-4">
