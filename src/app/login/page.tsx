45ba1a12f8f4e74e637837b777d1988bebbc3467
Sheraz Hussain <sherazhussainofficial2@gmail.com>
2025-10-27 18:47:04 +0000

    lets make sure once this button is clicked it should take the user to th

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,7 @@
-
 'use client';
 
 import { useRouter } from 'next/navigation';
 import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 
 export default function LoginPage() {
   const router = useRouter();
